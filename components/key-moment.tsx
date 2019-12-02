c8d8d16a5e731818b3770ab214b25c98a8a03250
Rick Hawkins <rick@rickhawkins.me.uk>
2019-12-02 21:30:58 +0000

    Adds KeyMoment component

---
+++ components/key-moment.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import {
+    TouchableHighlight,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+type MyProps = {
+    itemMinute: number;
+    itemDescription: string;
+    onPress: () => void;
+}
+
+type MyState = {
+
+}
+
+class KeyMoment extends React.Component<MyProps, MyState> {
+
+    render() {
+        return (
+            <TouchableHighlight
+                onPress={this.props.onPress}
+                underlayColor="#f1f1f1">
+                <View style={styles.keyMoment}>
+                    <Text style={styles.keyMomentText}>{this.props.itemMinute}</Text>
+                    <Text style={styles.keyMomentText}>{this.props.itemDescription}</Text>
+                </View>
+            </TouchableHighlight>
+        );
+    }
+}
+
+export default KeyMoment;
+
+const styles = StyleSheet.create({
+    keyMoment: {
+        flex: 1,
+        paddingTop: 10,
+        flexDirection: "row"
+    },
+    keyMomentText: {
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingTop: 10,
+        fontWeight: "normal",
+        fontSize: 16,
+        textAlign: "center"
+    },
+});
